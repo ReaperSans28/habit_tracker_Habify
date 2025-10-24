324c52d75ba4734b0d5f149097700258f6b5a566
ReaperSans28 <smileuwu321@gmail.com>
2025-10-24 19:49:22 +0300

    added handlers /add_habit, /habits

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from config import BOT_TOKEN
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
-from src.handlers import commands
+from src.handlers import commands, text, media
 
 async def main():
     """Запуск."""
@@ -12,6 +12,8 @@ async def main():
     dp = Dispatcher(storage=MemoryStorage())
 
     dp.include_router(commands.commands_router)
+    dp.include_router(text.text_router)
+    dp.include_router(media.media_router)
 
     await dp.start_polling(bot)
 
